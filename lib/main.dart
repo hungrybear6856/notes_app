3dda279ad6db9a8a90b784f125974b67f4cc85cb
hungrybear6856 <hungrybear6856@gmail.com>
2023-05-27 17:07:35 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('ddd'),
+      ),
+    );
+  }
+}
